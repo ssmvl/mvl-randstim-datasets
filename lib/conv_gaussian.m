ea401ee38660ed0f7058e6cd36b482050a1365c4
Oakyoon Cha <oakyoon@users.noreply.github.com>
2025-03-01 23:25:03 +0900

    Deprecate repository: Redirect users in README

--- lib/conv_gaussian.m
+++
@@ -1,7 +0,0 @@
-function N = conv_gaussian(M, sd, x0)
-	if ~exist('x0', 'var') || isempty(x0)
-		x0 = 0;
-	end
-
-	N = exp(-.5 * ((M - x0) ./ sd) .^ 2);
-end
--- zebra-continuous/zebraComp.m
+++
@@ -1,8 +0,0 @@
-function fftmat = zebraComp(tpl)
-	blobmat = conv_logistic(mapt_radial(tpl.imSize), -tpl.blobK, tpl.blobSize / 2);
-	srcmat = (rand(tpl.imSize) * 2 - 1) .* blobmat;
-	
-	sfFilt = conv_gaussian(map_radial(tpl.imSize), tpl.stdSF, tpl.meanSF);
-	sfFilt(floor(tpl.imSize / 2) + 1, floor(tpl.imSize / 2) + 1) = 0;
-	fftmat = fftshift(fft2(srcmat)) .* sfFilt;
-end
